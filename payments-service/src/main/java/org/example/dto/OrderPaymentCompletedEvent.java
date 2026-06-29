f3a737e65434f89749511decab7f4050dbfe7ae8
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2026-06-29 19:48:39 +0300

    orbita v1.0

--- payments-service/src/main/java/org/example/dto/OrderPaymentCompletedEvent.java
+++ payments-service/src/main/java/org/example/dto/OrderPaymentCompletedEvent.java
@@ -1,7 +1,12 @@
 package org.example.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.util.UUID;
 
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record OrderPaymentCompletedEvent(
         UUID eventId,
         UUID orderId,
--- payments-service/src/main/java/org/example/dto/OrderPaymentRequestedEvent.java
+++ payments-service/src/main/java/org/example/dto/OrderPaymentRequestedEvent.java
@@ -1,8 +1,12 @@
 package org.example.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record OrderPaymentRequestedEvent(
         UUID eventId,
         UUID orderId,

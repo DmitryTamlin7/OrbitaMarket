1157be89fad5827dc29a906d0be1371b31f94993
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2026-06-30 15:31:07 +0300

    Добавлен Allure отчет по нагрузкам и unit тестам

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     id("org.springframework.boot") version "3.2.5"
+    id("io.qameta.allure") version "3.0.1"
 }
 
 group = "org.example"
@@ -9,6 +10,10 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+val allureVersion = "2.24.0"
+val restAssuredVersion = "5.3.2"
+val junitVersion = "5.10.1"
 
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.5"))
@@ -20,8 +25,21 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.qameta.allure:allure-junit5:2.25.0")
 }
 
-tasks.withType<Test> {
+allure {
+    version.set(allureVersion)
+    adapter {
+        frameworks {
+            junit5 {
+                adapterVersion.set(allureVersion)
+            }
+        }
+    }
+}
+
+tasks.test {
     useJUnitPlatform()
+    systemProperty("allure.results.directory", layout.buildDirectory.dir("allure-results").get().asFile.absolutePath)
 }
